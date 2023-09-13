6c497c7a785b457ef2db9f9d194ea913b9a8fe7a
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2023-09-14 02:37:21 +0530

    cache bust test 2

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App = () => {
   return (
     <CacheBuster
       currentVersion={appVersion}
-      isEnabled={true} //If false, the library is disabled.
+      isEnabled={false} //If false, the library is disabled.
       isVerboseMode={true} //If true, the library writes verbose logs to console.
       loadingComponent={<Loading />} //If not pass, nothing appears at the time of new version check.
       metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
