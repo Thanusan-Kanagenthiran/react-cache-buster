b9521b15f8d247e5b1ce85e63351a2e2d7044c21
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2023-09-14 02:26:34 +0530

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import CacheBuster from 'react-cache-buster';
+import Home from './Home';
+import Loading from './Loading';
+
+const appVersion = require('../package.json').version;
+
+const App = () => {
+  // const isProduction = process.env.NODE_ENV === 'production';
+  return (
+    <CacheBuster
+      currentVersion={appVersion}
+      isEnabled={true} //If false, the library is disabled.
+      isVerboseMode={true} //If true, the library writes verbose logs to console.
+      loadingComponent={<Loading />} //If not pass, nothing appears at the time of new version check.
+      metaFileDirectory={'.'} //If public assets are hosted somewhere other than root on your server.
+      reloadOnDowngrade={true}
+    >
+
+      <Home />
+
+    </CacheBuster>
+  );
+};
+
+export default App;
---
+++ src/Home.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { useCacheBuster } from 'react-cache-buster';
+const Home = () => {
+    const { checkCacheStatus } = useCacheBuster();
+  return (
+    <div>Home
+        <br />
+         <button onClick={checkCacheStatus}>Check for new version</button>
+    </div>
+  )
+}
+
+export default Home
