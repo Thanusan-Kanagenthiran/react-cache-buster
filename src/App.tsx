714604b48f01e75f78ebad61602efd4f350c0cad
Thanusan-Kanagenthiran <thanushmk6@gmail.com>
2023-09-14 10:58:28 +0530

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,25 +3,27 @@ import React from 'react';
 import CacheBuster from 'react-cache-buster';
 import Home from './Home';
 import Loading from './Loading';
-import packageJson from '../package.json';
-declare global {
-  var appVersion: string;
-}
+// import packageJson from '../package.json';
+// declare global {
+//   var appVersion: string;
+// }
 
-global.appVersion = packageJson.version;
-console.log(appVersion);
+// global.appVersion = packageJson.version;
+// console.log(appVersion);
+
+const appVersion: any = "1.2.3"
 
 const App = () => {
-  const appVersion = global.appVersion;
+  // const appVersion = global.appVersion;
 
   return (
     <CacheBuster
       currentVersion={appVersion}
       isEnabled={true}
       isVerboseMode={true}
-      loadingComponent={<Loading />}
-      metaFileDirectory={'.'}
-      reloadOnDowngrade={true}
+      // loadingComponent={<Loading />}
+      // metaFileDirectory={'.'}
+      // reloadOnDowngrade={true}
     >
       <Home />
     </CacheBuster>
