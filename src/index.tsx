47947300215279a947d64527ce4c3ce1402380af
thanusankanagenthiran <thanusan.kanagenthiran@acentura.com>
2023-09-21 10:19:10 +0530

    into root element

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,31 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import CacheBuster from 'react-cache-buster';
+import packageJson from '../package.json';
+
+declare global {
+  var appVersion: string;
+}
+
+global.appVersion = packageJson.version;
+console.log(appVersion);
+
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+
+   <CacheBuster
+      currentVersion={appVersion}
+      isEnabled={true}
+      isVerboseMode={true}
+    >
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+  </CacheBuster>
 );
 
 // If you want to start measuring performance in your app, pass a function
